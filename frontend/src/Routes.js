1381220abd7bbbfa7ef58239723d0bdd6f8f5177
Amol Tangade <tangadeamol@gmail.com>
2023-06-14 23:17:14 +0530

    authenticate users from UI

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import NotFound from "./components/NotFound";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 export default function Links() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="*" element={<NotFound />} />;
     </Routes>
   );
