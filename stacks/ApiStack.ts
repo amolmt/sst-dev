a66b7cfbb5c34e3bf874f6fb7800a20cbc86088d
Amol Tangade <tangadeamol@gmail.com>
2023-06-14 20:06:04 +0530

    securing the API

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -7,6 +7,7 @@ export function ApiStack({ stack, app }: StackContext) {
   // Create the API
   const api = new Api(stack, "Api", {
     defaults: {
+      authorizer: "iam",
       function: {
         bind: [table],
       },
